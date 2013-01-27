364c52c207054622a439009335603f8bc7ed4471
Uwe Kamper <me@uwekamper.de>
2013-01-27 23:19:51 +0100

    zwischenstand

--- ArduinoFlicker.ino
+++ ArduinoFlicker.ino
@@ -9,8 +9,49 @@ int sensorValue = 0;  // variable to store the value coming from the sensor
 
 #define DETECTOR_DELAY 50 // in ms
 
-void edgeDetected() {
+long detectPreamble() {
+  long t_firstedge;
+  long t_secondedge;
+  long te = 0;
   
+  Serial.println("Waiting for preamble");
+  int i = 0;
+  while(true) {
+    // Serial.println(i);
+    int first_edge = waitForEdge();
+    t_firstedge = millis();
+    
+    if (first_edge != 1) {
+      i = 0;
+      continue;
+    }
+    
+    if( waitForEdge() != first_edge ) {
+      t_secondedge = millis();
+      // Serial.println("go");
+    }
+    else {
+      i = 0;
+      continue;
+    }
+    
+    i++;
+    te = (te + (t_secondedge - t_firstedge)) / 2;
+  
+    
+    if (i >= 8) {
+      Serial.println("got preamble");
+        Serial.print("TE = ");
+        Serial.println(te);
+    
+      break;   
+    }
+  }
+  return te;
+}
+
+decodeMessage() {
+  byte message = 0;
 }
 
 int waitForEdge() {
@@ -27,12 +68,14 @@ int waitForEdge() {
     dq = (secondVal - firstVal) / DETECTOR_DELAY;
     
     if (abs(dq) >= 1.0) {
-      Serial.print("Edge detected: ");
+      // Serial.print("Edge detected: ");
       if (dq > 0.0) {
-        Serial.println("^");
+        // Serial.println("^");
+        return 1;
       }
       else {
-        Serial.println("v");
+        // Serial.println("v");
+        return -1;
       }
     }
   } // end while
@@ -48,9 +91,8 @@ void setup() {
 
 void loop() {
   // read the value from the sensor:
-  
-  waitForEdge();
-  delay(250);
+  Serial.println("hello");
+  decodeMessage();
   // turn the ledPin on
        
 }
