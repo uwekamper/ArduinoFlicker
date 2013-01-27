eb5c100b745a4263a8b7f78376f3abf369b048a2
Uwe Kamper <me@uwekamper.de>
2013-01-27 20:25:44 +0100

    Edge detection works.

--- ArduinoFlicker.ino
+++ ArduinoFlicker.ino
@@ -7,6 +7,39 @@ int sensorPin = A0;    // select the input pin for the potentiometer
 int ledPin = 13;      // select the pin for the LED
 int sensorValue = 0;  // variable to store the value coming from the sensor
 
+#define DETECTOR_DELAY 50 // in ms
+
+void edgeDetected() {
+  
+}
+
+int waitForEdge() {
+
+  int firstVal = -1; 
+  int secondVal = -1;
+  float dq = 0; // differential quotient of 
+  
+  while(true) {
+    firstVal = analogRead(sensorPin);
+    delay(DETECTOR_DELAY);
+    secondVal = analogRead(sensorPin);
+    
+    dq = (secondVal - firstVal) / DETECTOR_DELAY;
+    
+    if (abs(dq) >= 1.0) {
+      Serial.print("Edge detected: ");
+      if (dq > 0.0) {
+        Serial.println("^");
+      }
+      else {
+        Serial.println("v");
+      }
+    }
+  } // end while
+  
+  return 0;
+}
+
 void setup() {
   // declare the ledPin as an OUTPUT:
   pinMode(ledPin, OUTPUT);
@@ -15,19 +48,9 @@ void setup() {
 
 void loop() {
   // read the value from the sensor:
-  sensorValue = analogRead(sensorPin);    
-  for(int i = 0; i < 10; i++) {
-    Serial.print(sensorValue);
-    Serial.println("");
-  }
-
-  // turn the ledPin on
   
-//  digitalWrite(ledPin, HIGH);  
-//  // stop the program for <sensorValue> milliseconds:
-//  delay(sensorValue);          
-//  // turn the ledPin off:        
-//  digitalWrite(ledPin, LOW);   
-//  // stop the program for for <sensorValue> milliseconds:
-//  delay(250);                  
+  waitForEdge();
+  delay(250);
+  // turn the ledPin on
+       
 }
